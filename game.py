af2e04ded995fe049e24f3eea65eec779b3576b5
Derek <derek.kelly27@gmail.com>
2016-04-27 19:35:32 -0600

    Adds room and dungeon generation!

--- game.py
+++ game.py
@@ -5,8 +5,25 @@ import libtcodpy as libtcod
 ##########################################################
 
 SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
+SCREEN_HEIGHT = 45
 LIMIT_FPS = 20
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+color_dark_wall = libtcod.Color(20, 20, 20)
+color_dark_ground = libtcod.Color(50,50,150)
+
+class Tile:
+	#A tile of the map and its properties
+	def __init__(self,blocked,block_sight = None):
+		self.blocked = blocked;
+
+		if block_sight is None: block_sight = blocked
+		self.block_sight = block_sight
 
 class Object:
 	#this is a generic object: a player, a monster, an item, stairs
@@ -18,8 +35,9 @@ class Object:
 		self.color = color
 
 	def move(self, dx, dy):
-		self.x += dx
-		self.y += dy
+		if not map[self.x + dx][self.y + dy].blocked:
+			self.x += dx
+			self.y += dy
 
 	def draw(self):
 		libtcod.console_set_default_foreground(con, self.color)
@@ -28,6 +46,116 @@ class Object:
 	def clear(self):
 		libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
+class Rect:
+	#a rectangle on the map, used to characterize a room
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+
+	def intersect(self, other):
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+			self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_room(room):
+	global map
+	#go through the tiles in the rectangle and make them passable
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			map[x][y].blocked = False
+			map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+	global map
+
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+	global map
+
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+def make_map():
+	global map
+
+	#fill map with "blocked" tiles
+	map = [[Tile(True)
+			for y in range(MAP_HEIGHT) ]
+				for x in range(MAP_WIDTH)]
+
+	rooms = []
+	num_rooms = 0
+
+	for r in range(MAX_ROOMS):
+		w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+		y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+		new_room = Rect(x, y, w, h)
+
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+
+		if not failed:
+			create_room(new_room)
+
+			(new_x, new_y) = new_room.center()
+
+			if num_rooms == 0:
+				player.x = new_x
+				player.y = new_y
+			else:
+				#all rooms after the first
+				#connect it to the previous room with a tunnel
+
+				#get center coordinates of previous room
+				(prev_x, prev_y) = rooms[num_rooms -1].center()
+
+				#flip a coin
+				if libtcod.random_get_int(0,0,1) == 1:
+					#first move horizontally, then vertically
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					#first move vertically, then horizontally
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+			#finally, append the new room to the list
+			rooms.append(new_room)
+			num_rooms += 1;
+
+
+
+def render_all():
+	global color_light_wall
+	global color_light_ground
+
+	#go through all tiles, set their background color
+	for y in range(MAP_HEIGHT):
+		for x in range(MAP_WIDTH):
+			wall = map[x][y].block_sight
+			if wall:
+				libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+			else:
+				libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+
+		for object in objects:
+			object.draw()
+
+		libtcod.console_blit(con, 0, 0 , SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
 def handle_keys():
 	global playerx, playery
@@ -60,18 +188,17 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 
 #create an NPC
-npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+#npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 
-objects = [npc, player]
+objects = [player]
+make_map()
 
 
 
 while not libtcod.console_is_window_closed():
 	
-	for object in objects:
-		object.draw()
+	render_all()
 
-	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 	libtcod.console_flush()
 
 	for object in objects:
